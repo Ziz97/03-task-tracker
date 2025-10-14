e96f7623124f759d28058e590696412bb6230c71
Abdelaziz EL OMARI ALAOUI <aelo@Mac.lan>
2025-10-14 23:45:41 +0200

    Handle Edit task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import ToDo from "./components/ToDo";
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-2xl font-bold py-4 pl-6">03 - The Task Tracker</h1>
+      <p className="text-xl pl-6">Hi there!</p> 
+      <div className="flex flex-row items-center">
+        <p className="text-xl pl-6">Click</p>
+        <AddTask taskList={taskList} setTaskList={setTaskList}/>
+        <p className="text-xl my-2">to add a new task</p>
+      </div>
+      <div>
+        <h2 className="ml-6 text-xl font-semibold w-3/4 max-w-lg my-4 py-2 px-2 bg-gray-300">To Do:</h2>
+        {taskList.slice(0).reverse().map((task, i) => (
+          <>
+            <ToDo key={new Date().getTime()} task={task} taskList={taskList} setTaskList={setTaskList} />
+          </>
+        ))}
+      </div>
+    </>
   );
 }
 
